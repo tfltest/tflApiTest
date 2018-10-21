ee60911ab2be6dbe0b18a6281f68934b1bb18c85
tfltest <jiten.panchal.uk@gmail.com>
2018-10-21 11:01:03 +0100

    Added Integration tests

---
+++ Tfl.Client.Commandline.IntegrationTests/BaseTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace Tfl.Client.Commandline.IntegrationTests
+{
+    public class BaseTests
+    {
+        protected readonly IDictionary<string, string> keys = new Dictionary<string, string>()
+        {
+            { "app_id", ConfigurationManager.AppSettings["app_id"] },
+            { "developer_key", ConfigurationManager.AppSettings["developer_key"] },
+            { "base_api_url", ConfigurationManager.AppSettings["base_api_url"] }
+        };
+    }
+}
---
+++ Tfl.Client.Commandline.IntegrationTests/Services/RoadServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Tfl.Client.Commandline.Contracts;
+using Tfl.Client.Commandline.Services;
+
+namespace Tfl.Client.Commandline.IntegrationTests
+{
+    [TestClass()]
+    public class RoadServiceTests : BaseTests
+    {
+        private IRoadService roadService;
+
+        [TestInitialize]
+        public void OnTestInitialize()
+        {
+            roadService = new RoadService(new ApplicationSettingsService(keys));
+        }
+
+        [TestCleanup]
+        public void OnTestCleanup()
+        {
+        }
+
+        [TestMethod()]
+        [DataRow("")]
+        [DataRow("   ")]
+        [DataRow(null)]
+        public async Task GetRoadStatusWithInvalidInputThrowsArgumentException(string id)
+        {
+            var argumentExcpetion = await Assert.ThrowsExceptionAsync<ArgumentException>(() => { return roadService.GetRoadStatus(id); });
+            Assert.IsTrue(argumentExcpetion.Message == nameof(id));
+        }
+
+        [TestMethod()]
+        [DataRow("A3")]
+        [DataRow("A2")]
+        public async Task GetRoadStatusWithValidInputShouldReturnRoadStatus(string id)
+        {
+            // Act
+            var apiResponse = await roadService.GetRoadStatus(id);
+
+            // Assert
+            Assert.IsTrue(apiResponse.IsSuccess);
+            Assert.IsNotNull(apiResponse.Response);
+            Assert.IsNull(apiResponse.ApiError);
+            Assert.IsTrue(apiResponse.Response.Count == 1);
+            Assert.IsTrue(apiResponse.Response.Single().Id.Equals(id, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        [TestMethod()]
+        [DataRow("A****3")]
+        [DataRow("A****2")]
+        public async Task GetRoadStatusWithNonExistentRoadIdShouldReturn404NotFoundError(string id)
+        {
+            // Act
+            var apiResponse = await roadService.GetRoadStatus(id);
+
+            // Assert
+            Assert.IsFalse(apiResponse.IsSuccess);
+            Assert.IsNull(apiResponse.Response);
+            Assert.IsNotNull(apiResponse.ApiError);
+            Assert.IsTrue(apiResponse.ApiError.HttpStatusCode == HttpStatusCode.NotFound);
+        }
+    }
+}
